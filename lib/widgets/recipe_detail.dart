e3adeef7467099178681db7f8f8732c6d5a21ab2
oozoemenam <oozoemenam@gmail.com>
2023-08-22 19:58:03 +0200

    add ingredients, servings slider

--- lib/widgets/recipe_detail.dart
+++ lib/widgets/recipe_detail.dart
@@ -11,6 +11,8 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
+  int _sliderVal = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +35,33 @@ class _RecipeDetailState extends State<RecipeDetail> {
             Text(
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  return Text('${ingredient.quantity * _sliderVal} ${ingredient.measure} ${ingredient.name}');
+                  return Text(
+                    '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}'
+                  );
+                },
+              ),
+            ),
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 9,
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              value: _sliderVal.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
             ),
           ],
         ),
